cebaa377cf0a60bb95507e0f4452cfd2490ddd55
Mauri Chamorro <Mauricio-Chamorro@hotmail.com>
2017-08-20 19:16:32 -0300

    antes del marge

--- Dominio/Program.cs
+++ Dominio/Program.cs
@@ -10,18 +10,7 @@ namespace Dominio
     {
         static void Main(string[] args)
         {
-            ControladorAlojamiento iControlador = new ControladorAlojamiento();
-
-            //instancias solo para prueba
-            Cliente lCliente = new Cliente(37115628, "Mauricio", "Chamorro", "3456542154", TipoCliente.Titular);
-            TarifaCliente lTarifaCliente = new TarifaCliente();
-            bool lExclusividad = true;
-
-            //---------------------------
-
-            Console.WriteLine(iControlador.ObteberTarifa(lCliente, lTarifaCliente, lExclusividad) );
-
-            Console.ReadKey();
+            
         }
     }
 }
