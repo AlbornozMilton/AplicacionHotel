c4088c15741e8cc0866db07aa9bd2d5cbf3ffb8c
Milton <Milton@DESKTOP-P6G4O5Q>
2017-10-24 22:34:33 -0300

    AutoMapper

--- Dominio/ControladorCliente.cs
+++ Dominio/ControladorCliente.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using AutoMapper;
+using Dominio.DTO_Mapper;
+using Persistencia.DAL.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +11,16 @@ namespace Dominio
 {
     public class ControladorCliente
     {
-        public Cliente NuevoCliente (string pDni, string pNombre, string pApellido, string pTel)
+        AutoMapperHotel Mapeo = new AutoMapperHotel();
+        UnitOfWork iUoW = new UnitOfWork(new HotelContext());
+        public void NuevoCliente(string pDni, string pNombre, string pApellido, string pTel)
         {
-            return (new Cliente(Convert.ToInt32(pDni),pNombre,pApellido,pTel));
+            Mapeo.Mapear();
+            Cliente cli = new Cliente(Convert.ToInt32(pDni), pNombre, pApellido, pTel);
+            var nuevoCli = Mapper.Map<Cliente, Persistencia.Domain.Cliente>(cli);
+            //Persistencia.Domain.Cliente cliente = Converter<Cliente, Persistencia.Domain.Cliente> Cliente;
+            iUoW.RepositorioCliente.Add(nuevoCli);
+            iUoW.Complete();
         }
     }
 }
