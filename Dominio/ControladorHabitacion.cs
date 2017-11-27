847066ef160958ab81a3c4bc609ed301b82aa6fc
Milton <Milton@DESKTOP-P6G4O5Q>
2017-11-26 21:49:52 -0300

    determinar

--- Dominio/ControladorHabitacion.cs
+++ Dominio/ControladorHabitacion.cs
@@ -1,54 +1,97 @@
-﻿using Persistencia.DAL.EntityFramework;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Persistencia.DAL.EntityFramework;
+
+
 
 namespace Dominio
 {
     public class ControladorHabitacion
     {
-        UnitOfWork iUoW = new UnitOfWork(new HotelContext());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         public List<Habitacion> DeterminarDisponibilidad(DateTime fechaDesde, DateTime fechaHasta)
         {
+            DateTime fechaIni = new DateTime();
+            DateTime fechaFin = new DateTime();
             List<Alojamiento> listaAlojActivos = new List<Alojamiento>(); //lista de alojamientos en estado de Alojado o Reservado
-            List<Habitacion> listaHabitaciones = new List<Habitacion>(); //lista de todas las habitaciones del hotel, solo los HabitacionesID
-            List<Habitacion> listaHabDisponibles = new List<Habitacion>(); //lista de habitaciones con cupos disponibles en esas fechas
+            List<Habitacion> listaHabitaciones = new List<Habitacion>(); //GENERAR LISTA DE HABITACION TODAS LIBRE (GET ALL CON REPOSITORY)lista de todas las habitaciones del hotel, solo los HabitacionesID
+            //List<Habitacion> listaHabDisponibles = new List<Habitacion>(); //lista de habitaciones con cupos disponibles en esas fechas
             foreach (var hab in listaHabitaciones)
             {
                 foreach (var aloj in listaAlojActivos)
                 {
-                    if (aloj.Habitacion.HabitacionId == hab.HabitacionId)
+                    if (aloj.Habitacion.HabitacionId == hab.HabitacionId) //agregar condicion de exclusividad
                     {
-                        if (aloj.EstadoAlojamiento == EstadoAlojamiento.Alojado)
+                        if (aloj.Habitacion.Exclusiva == true)
                         {
-                            //DateTime fechaIni = aloj.getfechaIngreso()
-                            //DateTime fechaFin = aloj.getfechaEstimadaEgreso();
+                            listaHabitaciones.Remove(hab);
                         }
-                        else
+                        else if (aloj.EstadoAlojamiento == EstadoAlojamiento.Alojado)
                         {
-                            //DateTime fechaIni = aloj.getfechaEstimadaIngreso();
-                            //DateTime fechaFin = aloj.getfechaEstimadaEgreso();
+                            fechaIni = aloj.FechaIngreso;
+                            fechaFin = aloj.FechaEstimadaEgreso;
                         }
-                        if (fechaDesde>fechaFin) || (fechaHasta<fechaIni)
+                        else if(aloj.EstadoAlojamiento == EstadoAlojamiento.Reservado)
                         {
+                            fechaIni = aloj.FechaEstimadaIngreso;
+                            fechaFin = aloj.FechaEstimadaEgreso;
+                        }
+
+                        if ((DateTime.Compare(fechaDesde,fechaFin)<0) || ((DateTime.Compare(fechaHasta,fechaIni)>1)))
+                           //si las fechas son iguales la habitacion estaria desponible full ya que el check out es a las 10
+                        {
+                            //modificar la habitacion de la lista de acuerdo a su ocupacion actual para luego sacar lo libre
                             //obtenerCantidadCuposDoble()
                             //obtenerCantidadCuposSimples()
                             //determinarExclusividad()
                             //listaHabDisponlibles.Add(hab)
                         }
                     }
-                    else
-                    {
-                        //obtenerCantidadCuposDoble()
-                        //obtenerCantidadCuposSimples()
-                        //determinarExclusividad()
-                        //listaHabDisponlibles.Add(hab)
-                    }
                 }
             }
-            return (listaHabDisponibles);
+            return (listaHabitaciones);
         }
     }
 }
