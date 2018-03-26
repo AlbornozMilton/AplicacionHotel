2ab510678a20c25c80e9bea29ba504c8c666b7c1
MauriChamorro <mmchamoo@gmail.com>
2018-03-26 16:38:07 -0300

    Eliminando using

--- Persistencia/DAL/EntityFramework/HotelContext.cs
+++ Persistencia/DAL/EntityFramework/HotelContext.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Data.Entity;
+﻿using System.Data.Entity;
 using Persistencia.Domain;
 using Persistencia.DAL.EntityFramework.Mappings;
 using System.Data.Entity.ModelConfiguration.Conventions;
--- Persistencia/DAL/EntityFramework/Mappings/UsuarioMap.cs
+++ Persistencia/DAL/EntityFramework/Mappings/UsuarioMap.cs
@@ -1,10 +1,5 @@
 ﻿using Persistencia.Domain;
-using System;
-using System.Collections.Generic;
 using System.Data.Entity.ModelConfiguration;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Persistencia.DAL.EntityFramework.Mappings
 {
