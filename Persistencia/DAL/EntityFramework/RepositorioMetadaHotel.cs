479e5bb9431e9b0d8d9eb1e8954113cbfaa144cf
MauriChamorro <mmchamoo@gmail.com>
2018-04-04 00:37:31 -0300

    repositorio Metadata

---
+++ Persistencia/DAL/EntityFramework/RepositorioMetadaHotel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Persistencia.Domain;
+
+namespace Persistencia.DAL.EntityFramework
+{
+	public class RepositorioMetadaHotel: Repositorio<MetadataHotel, HotelContext>, IRepositorioMetadaHotel
+	{
+		public RepositorioMetadaHotel(HotelContext pContext) : base(pContext)
+		{
+
+		}
+
+		public int ObtenerValorMetada(byte pNroMetadata)
+		{
+			return iDbContext.Metadatas.Find(pNroMetadata).Valor;
+		}
+	}
+}
---
+++ Persistencia/DAL/IRepositorioMetadaHotel.cs
@@ -0,0 +1,9 @@
+﻿using Persistencia.Domain;
+
+namespace Persistencia.DAL
+{
+	public interface IRepositorioMetadaHotel:IRepositorio<MetadataHotel>
+	{
+		int ObtenerValorMetada(byte pNroMetadata);
+	}
+}
